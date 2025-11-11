f725d32fb001c127f987f778896e491916cb98d6
Weiyi Lee <weiyilee17@gmail.com>
2025-11-11 16:04:08 +0800

    Fix lint

--- src/env.js
+++ src/env.js
@@ -27,7 +27,6 @@ export const env = createEnv({
   client: {
     // NEXT_PUBLIC_CLIENTVAR: z.string(),
     NEXT_PUBLIC_POSTHOG_KEY: z.string(),
-    NEXT_PUBLIC_POSTHOG_HOST: z.string(),
   },
 
   /**
@@ -45,7 +44,6 @@ export const env = createEnv({
     SINGLESTORE_DB_NAME: process.env.SINGLESTORE_DB_NAME,
     UPLOADTHING_TOKEN: process.env.UPLOADTHING_TOKEN,
     NEXT_PUBLIC_POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY,
-    NEXT_PUBLIC_POSTHOG_HOST: process.env.NEXT_PUBLIC_POSTHOG_HOST,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
